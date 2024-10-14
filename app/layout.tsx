15d06dc653ea104313e3fed81aece7ade45f2e55
someTrickyCase <snowinboots18@gmail.com>
2024-10-14 18:14:40 +0400

    dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,23 @@ import { Jost } from "next/font/google";
 import "./globals.css";
 
 const font = Jost({
-  subsets: ["latin", "cyrillic"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    subsets: ["latin", "cyrillic"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-  title: "TroffiRU",
-  description: "Тюнинг для внедорожников",
+    title: "Hilux Toyota",
+    description: "Специализированный магазин тюнинга для Toyota Hilux",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en'>
-      <body className={`${font} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={`${font} antialiased`}>{children}</body>
+        </html>
+    );
 }
